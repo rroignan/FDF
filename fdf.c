e181907e2611b65e35db2e14bc666399ed219488
Robin ROIGNANT <rroignan@e1r4p21.42.fr>
2015-05-30 17:44:12 +0200

    plus de segfault

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: rroignan <rroignan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/16 18:14:44 by rroignan          #+#    #+#             */
-/*   Updated: 2015/05/28 22:21:25 by rroignan         ###   ########.fr       */
+/*   Updated: 2015/05/30 17:43:47 by rroignan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,13 @@ char	***parse(char ***tab, char *str)
 	char	*line;
 
 	i = 0;
+	fd = open(str, O_RDONLY);
+	while (get_next_line(fd, &line))
+		i++;
+	if ((tab = (char ***)malloc(sizeof(char **) * (i + 1))) == NULL)
+		return (0);
+	i = 0;
+	close(fd);
 	fd = open(str, O_RDONLY);
 	while (get_next_line(fd, &line))
 	{
@@ -91,26 +98,37 @@ void	ft_draw(void *mlx, void *win, int x1, int y1, int x2, int y2, int color)
 			ft_drawy(mlx, win, x1, y1, x2, y2, color);
 	}
 	/*if ((x2 - x1) > (y2 - y1))
-	{
-			while (x <= x2)
-			{
-				mlx_pixel_put(mlx, win, x, y1 + ((y2 - y1) * (x - x1)) / (x2 - x1), color);
-				x++;
-			}
-	}
-	else
-	{
-			while (y <= y2)
-			{
-				mlx_pixel_put(mlx, win, x1 + ((x2 - x1) * (y - y1)) / (y2 - y1), y, color);
-				y++;
-			}
-	}*/
+	  {
+	  while (x <= x2)
+	  {
+	  mlx_pixel_put(mlx, win, x, y1 + ((y2 - y1) * (x - x1)) / (x2 - x1), color);
+	  x++;
+	  }
+	  }
+	  else
+	  {
+	  while (y <= y2)
+	  {
+	  mlx_pixel_put(mlx, win, x1 + ((x2 - x1) * (y - y1)) / (y2 - y1), y, color);
+	  y++;
+	  }
+	  }*/
+}
+
+int		ft_strlen2(char **str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
 void	ft_show(char ***tab, t_env *e)
 {
-	int		i;
+	size_t	i;
+	int		boolie;
 	int		j;
 	int		x;
 	int		y;
@@ -120,28 +138,43 @@ void	ft_show(char ***tab, t_env *e)
 	while (tab[i + 1])
 	{
 		j = 0;
-		x = 400 - (i * XSIZE);
-		y = 100 + (i * YSIZE);
+		boolie = 1;
+		x = 900 - (i * XSIZE);
+		y = 200 + (i * YSIZE);
 		while (tab[i][j + 1])
 		{
-			if (ft_atoi(tab[i][j]) != 0 || ft_atoi(tab[i][j + 1]) != 0)
-				ft_draw(e->mlx, e->win, x, y - (ft_atoi(tab[i][j]) * H), x + XSIZE, y - (ft_atoi(tab[i][j + 1]) * H) + YSIZE, 0xFF0000);
-			else
-				ft_draw(e->mlx, e->win, x, y - (ft_atoi(tab[i][j]) * H), x + XSIZE, y - (ft_atoi(tab[i][j + 1]) * H) + YSIZE, 0x0000FF);
-			if (ft_atoi(tab[i][j]) != 0 || ft_atoi(tab[i + 1][j]) != 0)
-				ft_draw(e->mlx, e->win, x, y - (ft_atoi(tab[i][j]) * H), x - XSIZE, y - (ft_atoi(tab[i + 1][j]) * H) + YSIZE, 0xFF0000);
-			else
-				ft_draw(e->mlx, e->win, x, y - ft_atoi(tab[i][j]) * H, x - XSIZE, y - ft_atoi(tab[i + 1][j]) * H + YSIZE, 0x0000FF);
+			if ((ft_atoi(tab[i][j]) != 0 || ft_atoi(tab[i][j + 1]) != 0))
+				ft_draw(e->mlx, e->win, x, y - (ft_atoi(tab[i][j]) * H), x + XSIZE, y -
+						(ft_atoi(tab[i][j + 1]) * H) + YSIZE, 0xFF0000);
+			else if (ft_atoi(tab[i][j]) == 0 && ft_atoi(tab[i][j + 1]) == 0)
+				ft_draw(e->mlx, e->win, x, y - (ft_atoi(tab[i][j]) * H), x + XSIZE, y -
+						(ft_atoi(tab[i][j + 1]) * H) + YSIZE, 0x0000FF);
+			if (!tab[i + 1][j])
+				boolie = 0;
+			if (boolie)
+			{
+				if (tab[i + 1][j] && (ft_atoi(tab[i][j]) != 0 || ft_atoi(tab[i + 1][j]) != 0))
+					ft_draw(e->mlx, e->win, x, y - (ft_atoi(tab[i][j]) * H), x - XSIZE, y -
+							(ft_atoi(tab[i + 1][j]) * H) + YSIZE, 0xFF0000);
+				else if (tab[i + 1][j] && ft_atoi(tab[i][j]) == 0 && ft_atoi(tab[i + 1][j]) == 0)
+				{
+					ft_draw(e->mlx, e->win, x, y - ft_atoi(tab[i][j]) * H, x - XSIZE, y -
+							ft_atoi(tab[i + 1][j]) * H + YSIZE, 0x0000FF);
+				}
+			}
 			j++;
 			y += YSIZE;
 			x += XSIZE;
 		}
-		ft_draw(e->mlx, e->win, x, y - (ft_atoi(tab[i][j]) * H), x - XSIZE, y - ft_atoi(tab[i + 1][j]) * H + YSIZE, 0x0000FF);
+		if (ft_strlen2(tab[i]) <= ft_strlen2(tab[i + 1]))
+			ft_draw(e->mlx, e->win, x, y - (ft_atoi(tab[i][j]) * H), x - XSIZE, y -
+					ft_atoi(tab[i + 1][j]) * H + YSIZE, 0x0000FF);
 		i++;
 	}
-	x = 400 - (i * XSIZE);
-	y = 100 + i * YSIZE;
+	x = 900 - (i * XSIZE);
+	y = 200 + i * YSIZE;
 	j = 0;
+	(void)e;
 	while (tab[i][j + 1])
 	{
 		ft_draw(e->mlx, e->win, x, y - (ft_atoi(tab[i][j]) * H), x + XSIZE, y - (ft_atoi(tab[i][j + 1]) * H) + YSIZE, 0x0000FF);
@@ -156,28 +189,19 @@ int		main(int ac, char **av)
 	char	***tab;
 	t_env	*e;
 
+	tab = NULL;
 	e = malloc(sizeof(t_env));
 	ft_bzero(e, sizeof(t_env));
 	if (ac != 2)
 		return (0);
 	e->mlx = mlx_init();
-	e->win = mlx_new_window(e->mlx, 1000, 800, "42");
-	if ((tab = (char ***)malloc(sizeof(char **) * 400)) == NULL)
-		return (0);
+	e->win = mlx_new_window(e->mlx, 2200, 2200, "42");
+	//if ((tab = (char ***)malloc(sizeof(char **) * 400)) == NULL)
+	//	return (0);
 	tab = parse(tab, av[1]);
+
 	ft_show(tab, e);
 	mlx_hook(e->win, KEY_PRESS, KEY_PRESS_MASK, key_hook, &e);
 	mlx_loop(e->mlx);
 	return (0);
 }
-			/*ft_putstr("x: ");
-			ft_putnbr(x);
-			ft_putchar('~');
-			ft_putnbr(x + XSIZE);
-			ft_putendl(" ");
-			ft_putstr("y: ");
-			ft_putnbr(y - ft_atoi(tab[i][j]));
-			ft_putchar('*');
-			ft_putnbr(y - ft_atoi(tab[i][j + 1]));
-			ft_putchar('&');
-			ft_putchar('\n');*/
